98cb17aba8b4f767b5f118f7e91ecd3535b0b884
Dmytro Yashkir <dima.yashkir@gmail.com>
2016-12-09 13:10:57 +0900

    Initial Commit

---
+++ NeuralNet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NeuralNet
+//
+//  Created by Dmytro Yashkir on 2016-12-09.
+//  Copyright © 2016 Dmytro Yashkir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
